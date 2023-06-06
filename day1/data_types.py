d42ec64563c4c5d45b005924a5e24bd37f7b74b4
Han Zaw Nyein <hanzawnyein@192.168.1.4>
2023-06-06 21:26:56 +0630

    [add] day1 uploaded

---
+++ day1/data_types.py
@@ -0,0 +1,13 @@
+# variable
+# variable_name = value
+# name = 100.0 # str, int, float
+# print(type(name))
+
+# type casting
+# name = float(1000)
+# print(type(name),name)
+
+# sequence data types
+# item collection
+# data = [1,2,3,4,"Han","Zaw","Nyein"]
+# print(type(data),data)
---
+++ day1/main.py
@@ -0,0 +1,15 @@
+# standard input/output
+
+# output
+
+name = "Han Zaw Nyein"
+age = 100
+# index number - 0,1,2,3,4
+print("My name is {name}. My age is {age}".format(name="han",age=100))
+print(f"My name is {name}. My age is {age}")
+
+print("My age is %.2f" % age)
+
+# input
+name = input("Enter your name : ")
+print(f"Hello, {name}")
