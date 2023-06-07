9a2c719688b6abf822f870d4d049e654d14a6d5b
Han Zaw Nyein <hanzawnyein@192.168.1.3>
2023-06-07 20:44:00 +0630

    [day2] uploaded slicing power, data types, string function, buildin methods

---
+++ day2/main.py
@@ -0,0 +1,35 @@
+#  Standard Data Type
+
+# Numbers
+# string
+# list
+# tuple
+# dict
+# set
+
+
+# a = 1
+# del a #delete
+# print(a)
+
+# a = 10.005
+# print(type(a))
+
+# a = 3+4j #square root of -1
+# print(type(a))
+
+# boolean - true false
+# false - 0
+#  true - !0
+
+# print(bool(-2))
+
+#  type conversion
+# implicit ,  explicit
+
+# a = 10
+# b = 20.0
+# print(type(a+b))
+
+# a = 10.04
+# print(int(a))
---
+++ day2/slicing_power.py
@@ -0,0 +1,17 @@
+#   0123456789
+# name = "Han Zaw Nyein".upper()
+
+#  slicing power - [start:stop:jump]
+#  upper(), lower(), title()
+#  isupper(), islower()
+# count(),find(), replace()
+
+# print(name.title())
+# print(name.islower())
+
+# name = "Han Zaw Nyein"
+# new_name = name.replace("Ny","MI")
+# print(new_name)
+# print(name.count("Ny"))
+# print(name.find("J")) # index number
+# print(name[name.find("N")])
