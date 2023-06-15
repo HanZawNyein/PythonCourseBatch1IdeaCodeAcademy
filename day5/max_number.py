c9a92edf21d754d43649f3c00559fe022c1d24c1
Han Zaw Nyein <hanzawnyein@192.168.1.4>
2023-06-15 20:47:41 +0630

    [add] max number

---
+++ day5/max_number.py
@@ -0,0 +1,18 @@
+a = 70
+b = 25
+
+# c = [a,b]
+# # c.sort() # asc A --> Z 0 --> 9
+# c.sort(reverse=True) # desc Z --> A, 9 --> 0
+# print(c[0])
+# print(c[])
+
+# print(a if a >= b else b)
+
+# if a >= b:
+#     print(a)
+# else:
+#     print(b)
+
+# print(max(a,b))
+# print(min(a,b))
